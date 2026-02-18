04b774e4f542f5bca99d1bd8e9468f6d1644c411
Jass Dadwal <jassdadwal01@gmail.com>
2026-02-18 16:06:52 +0530

    Updated LoginTest and added gitignore

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -20,6 +20,7 @@ public class LoginTest {
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
         loginPage = new LoginPage(driver);
+        System.out.println("Login test Executed after this");
     }
    
  
